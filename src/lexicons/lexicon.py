009942f1c98d4b7549395b9cbbc9512a5077ef3a
GeorgiyKuzora <rafale87@gmail.com>
2023-04-23 09:50:06 +0300

    Added docstrings and formated lexicon

--- src/lexicons/lexicon.py
+++ src/lexicons/lexicon.py
@@ -1,36 +1,43 @@
 class LexiconRu:
     def get_start_command_answer(self) -> str:
         """Метод возвращает ответ на комманду /start"""
-        return ("Привет!\nДоступны следующие комманды:\n"
-                "/region - установить свой регион\n"
-                "/keyword - получить вакансии по ключевому слову")
+        return (
+            "Привет!\nДоступны следующие комманды:\n"
+            "/region - установить свой регион\n"
+            "/keyword - получить вакансии по ключевому слову"
+        )
 
     def get_region_command_answer(self, region: str) -> str:
         """Метод возвращает ответ на комманду /region"""
-        return (f"Вы задали регион: {region}")
+        return f"Вы задали регион: {region}"
 
     def get_invalid_region_command_answer(self) -> str:
         """Метод возвращает ответ на неверную комманду /region"""
-        return ("Вы не указали название региона")
+        return "Вы не указали название региона"
 
     def get_keyword_command_answer(self, keyword: str) -> str:
         """Метод возвращает ответ на комманду /keyword"""
-        return (f"Вы задали ключевое слово: {keyword}")
+        return f"Вы задали ключевое слово: {keyword}"
 
     def get_invalid_keyword_command_answer(self) -> str:
         """Метод возвращает ответ на неверную комманду /keyword"""
-        return ("Вы не указали ключевое слово")
+        return "Вы не указали ключевое слово"
 
     def get_invalid_command_answer(self) -> str:
         """Метод возвращает ответ на неверную комманду"""
-        return ("Команда не распознана")
+        return "Команда не распознана"
 
     def get_keyword_command_results(self, vacancy_data: list[tuple]) -> str:
-        get_messages = list(map(self.concat_vacancy_results, vacancy_data) )
+        """Метод возвращает строку с информацией о вакансиях
+        по запрошенному ключевому слову"""
+        get_messages = list(map(self.concat_vacancy_results, vacancy_data))
         get_messages = "\n".join(get_messages)
         return get_messages
 
     def concat_vacancy_results(self, vacancy: tuple) -> str:
+        """Метод соединяет информацию о вакансии в строку"""
         answer_message: str = ""
-        answer_message+="f{vacancy[1]}\n{vacancy[2]}\n{vacancy[3]}\n{vacancy[4]}\n{vacancy[5]}\n\n"
+        answer_message += (
+            f"{vacancy[1]}\n{vacancy[2]}\n{vacancy[3]}\n{vacancy[4]}\n{vacancy[5]}\n\n"
+        )
         return answer_message
