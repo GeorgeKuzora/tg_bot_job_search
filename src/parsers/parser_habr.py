ad0fa628749ae3352c2ee5023ca91111c57bbf0e
Olga Ermolaeva <photo-deti@mail.ru>
2023-04-26 10:45:12 +0700

    fixed bug parser

--- src/parsers/parser_habr.py
+++ src/parsers/parser_habr.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import fake_useragent
-import time
-#import json
 
 
 region = {"Москва": "c_678",
@@ -15,13 +13,13 @@ JOB = "java"
 CURRENT_REGION = region["Питер"]
 
 
-def get_useragent() -> None:
+def get_useragent() -> fake_useragent.fake.FakeUserAgent:
     """Генерирует фейковый заголовок"""
     return fake_useragent.UserAgent()
 
 
-def make_response(current_url: str):
-    """Создает запрос к списку вакансий"""
+def make_response(current_url: str) -> requests.models.Response:
+    """Создает запрос веб странице"""
     ua = get_useragent()
     response = requests.get(
         url=current_url,
@@ -30,9 +28,9 @@ def make_response(current_url: str):
     return response
 
 
-def make_link_career(current_region: str, page: int, job: str) -> str:
+def make_link_career(page: int) -> str:
     """Формирует url списка вакансий"""
-    return f"{URL}/vacancies?locations[]={current_region}&page={page}&q={job}&type=all"
+    return f"{URL}/vacancies?locations[]={CURRENT_REGION}&page={page}&q={JOB}&type=all"
 
 
 def find_vacancy_card(response) -> list:
@@ -40,7 +38,7 @@ def find_vacancy_card(response) -> list:
     link_list = []
     soup = create_beautiful_soup(response)
     if not soup.find('a', attrs={"class":"vacancy-card__title-link"}):
-        return
+        return link_list
     for link in soup.find_all('a', attrs={"class":"vacancy-card__title-link"}):
         link_list.append(f"{URL}{link.get('href')}")
     return link_list
@@ -59,22 +57,22 @@ def create_beautiful_soup(response) ->BeautifulSoup:
     return BeautifulSoup(response.content, "lxml")
 
 
-def get_links(current_region: str, job: str) -> list:
+def get_links() -> list:
     """Соединяет списки ваканский по всем страницам"""
     all_link_list = []
     i = 1
-    response = make_response(make_link_career(current_region, i, job))
-    if check_connection(response) != True:
-        return
-    while find_vacancy_card(response) != None:
-        response = make_response(make_link_career(current_region, i, job))
+    response = make_response(make_link_career(i))
+    if check_connection(response) == False:
+        return all_link_list
+    while find_vacancy_card(response) != []:
+        response = make_response(make_link_career(i))
         if check_connection(response):
-           if find_vacancy_card(response) != None:
+           if find_vacancy_card(response) != []:
              all_link_list.extend(find_vacancy_card(response))
         else:
-            print("На этом месте будет обработка ошибок")
+            pass
         i += 1
-        time.sleep(1)
+
     return all_link_list
 
 
@@ -82,12 +80,13 @@ def get_company(link: str) -> tuple:
     """Создание кортежа данных по вакансии"""
     response = make_response(link)
     if not check_connection(response):
-        return
+        return ()
     soup = create_beautiful_soup(response)
-    try:
+
+    if not soup.find('div', attrs={"class":"basic-salary basic-salary--appearance-vacancy-header"}):
+         salary = ""
+    else:
         salary = soup.find('div', attrs={"class":"basic-salary basic-salary--appearance-vacancy-header"}).text
-    except:
-        salary = ""
     tuple_job =(
                    soup.find('h1', attrs={"class":"page-title__title"}).text,
                    soup.find('div', attrs={"class":"company_name"}).text,
@@ -114,4 +113,4 @@ def get_list_company(all_link_list: list) -> list:
 
 if __name__=="__main__":
 
-    print(get_list_company(get_links(CURRENT_REGION,JOB)))
+    print(get_list_company(get_links()))
