07068e3413b1595d38c9aa0c66a5f316aed37ed0
GeorgiyKuzora <rafale87@gmail.com>
2023-05-04 21:10:56 +0300

    Created mock StorageInterface class
    Created mock StorageInterface class, for temporal use in interactor module.

---
+++ src/interactors/external_interface.py
@@ -0,0 +1,12 @@
+class StorageInterface:
+    @staticmethod
+    def write_user_data(user_data: tuple) -> None:
+        pass
+
+    @staticmethod
+    def write_region_data(region_data: tuple) -> None:
+        pass
+
+    @staticmethod
+    def get_data_for_keyword(keyword_data: tuple) -> tuple:
+        return ()
