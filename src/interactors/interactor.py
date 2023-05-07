c17bb2cb52060f0cc313989ac82662fe3f2e7ede
GeorgiyKuzora <rafale87@gmail.com>
2023-05-07 10:56:30 +0300

    Added exception handling to interactor get_vacancies_list method

--- src/interactors/interactor.py
+++ src/interactors/interactor.py
@@ -17,8 +17,15 @@ class Interactor:
             print(f"{er}, {type(er)}, Can't get access to data storage")
 
     def get_vacancy_list(self, keyword_data: tuple) -> list[tuple]:
-        vacancy_request_data: tuple = self._get_data_for_vacancies_request(keyword_data)
-        return ci.get_vacancy_list(vacancy_request_data)
+        try:
+            vacancy_request_data: tuple = self._get_data_for_vacancies_request(
+                keyword_data
+            )
+            return ci.get_vacancy_list(vacancy_request_data)
+        except StorageAccessException:
+            return [("Can't access data storage",)]
+        except RuntimeError:
+            return [("Can't access job servicies",)]
 
     def _get_data_for_vacancies_request(self, keyword_data: tuple) -> tuple:
         data_for_keyword: tuple = si.get_data_for_keyword(keyword_data)
