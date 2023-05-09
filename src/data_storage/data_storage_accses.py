67d3bd4021d2159aa888c4bbc4298e29d06c0836
Olga Ermolaeva <photo-deti@mail.ru>
2023-05-09 10:47:08 +0700

    Create conversion of a list of tuples into a json object

--- src/data_storage/data_storage_accses.py
+++ src/data_storage/data_storage_accses.py
@@ -7,27 +7,42 @@ class Vacancy:
         self.solary = solary
         self.link = link
 
-# def read_tg_data(user_data: tuple) -> None:
-#     """Считывание данных пользователя полученные от тг бота"""
-#     pass
+def read_tg_data(user_data: tuple) -> None:
+    """Считывание данных пользователя полученные от тг бота"""
+    pass
 
 
-# def save_tg_data():
-#     """Запись данных пользователя в папку data в формате json"""
-#     pass
+def save_tg_data():
+   """Запись данных пользователя в папку data в формате json"""
+   pass
+
+def read_list_vacancy(list_company: list):
+     """Считывание списка вакансий из парсера"""
+     for i in range(len(list_company)):
+        new_vacancy = create_vacancy_from_tuple(list_company[i])
+        json_str = create_json_str(new_vacancy)
+        print(json_str)
 
 
-# def read_parser_data(parser_data: list) -> tuple:
-#     """Считывание списка кортежей парсера"""
+def create_vacancy_from_tuple(vacancy_data: tuple) -> str:
+     """Создание объекта класса Vacancy из кортежа"""
+     return  Vacancy(vacancy_data[0], vacancy_data[1], vacancy_data[2], vacancy_data[3])
 
 
-# def tuple_from_list():
-#     #json_obj = json.dumps(tup)
-#     pass
+def create_json_str(new_vacancy) -> str:
+        """Создание json строки из объекта класса Vacancy"""
+        json_str = json.dumps(new_vacancy.__dict__,
+                              sort_keys=False,
+                             indent=4,
+                             ensure_ascii=False,
+                             separators=(',', ': '))
+        return json_str
 
-# def save_tg_data():
-#     """Запись данных парсера в папку data в формате json"""
-#     pass
+def save_json_in_file(json_str: str):
+     """Сохранение строки json в файл"""
+     with open("sample.json", "w") as outfile:
+        json.dump(json_str, outfile)
+
 
 if __name__ == "__main__":
 
@@ -39,8 +54,4 @@ if __name__ == "__main__":
                 ('Разработчик Java', 'TINKOFF', '', 'https://career.habr.com/vacancies/1000121969'),
                 ('Java-разработчик', 'Bell Integrator', '', 'https://career.habr.com/vacancies/1000115597')]
 
-    for i in range(len(list_vacancy)):
-        json_obj = json.dumps(list_vacancy[i])
-
-    with open("mydata.json", "w") as file:
-        json.dump(list_company, file)
+    read_list_vacancy(list_vacancy)
