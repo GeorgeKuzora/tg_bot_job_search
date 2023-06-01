78218fdbeebab601577a1ea56dea73f4c4cb6f95
GeorgiyKuzora <rafale87@gmail.com>
2023-06-01 19:17:21 +0300

    Added dataclasses for request types in controller module

--- src/controllers/tg_controller.py
+++ src/controllers/tg_controller.py
@@ -1,7 +1,72 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any, Protocol
 from src.interactors.interactor import Interactor
 
 
-class Controller:
+@dataclass
+class User:
+    id: str
+    username: str | None
+
+
+@dataclass
+class Request(Protocol):
+
+    @property
+    def date(self) -> datetime: ...
+
+    @property
+    def user(self) -> User: ...
+
+    @property
+    def value(self) -> Any: ...
+
+    def parse_request(self) -> dict: ...
+
+
+@dataclass
+class Fullname:
+    date: datetime
+    user: User
+    value: str | None
+
+    def parse_request(self) -> dict:
+        return {
+            "date": self.date,
+            "id": self.user.id,
+            "fullname": self.value,
+        }
+
+
+@dataclass
+class Region:
+    date: datetime
+    user: User
+    value: str | None
+
+    def parse_request(self) -> dict:
+        return {
+            "date": self.date,
+            "id": self.user.id,
+            "region": self.value,
+        }
+
+
+@dataclass
+class Keyword:
+    date: datetime
+    user: User
+    value: str | None
+
+    def parse_request(self) -> dict:
+        return {
+            "date": self.date,
+            "id": self.user.id,
+            "keyword": self.value,
+        }
+
+
     """Класс для задания методов работы контроллера"""
 
     @staticmethod
