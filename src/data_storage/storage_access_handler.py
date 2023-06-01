980089fc3746b0f16e41360fb835ed82efee9d36
GeorgiyKuzora <rafale87@gmail.com>
2023-06-01 19:26:00 +0300

    Changed naming in storage access handler

--- src/data_storage/storage_access_handler.py
+++ src/data_storage/storage_access_handler.py
@@ -1,10 +1,11 @@
 from src.errors.errors import StorageAccessException
+from . import data_storage_accses as si
 
 
 class Handler:
-    def set_user_data(self, user_data: tuple):
+    def set_data(self, user_data: dict):
         """Метод для записи данных пользователя в систему хранения"""
         try:
-            si.write_user_data(user_data)
+            si.write_data(user_data)
         except StorageAccessException as er:
             print(f"{er}, {type(er)}, Can't get access to data storage")
